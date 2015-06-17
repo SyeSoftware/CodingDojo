4e45e4c10e459717eead2fcf4c8d225b0c17b407
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2015-06-17 09:52:39 -0500

    Adding javascript specs and code

---
+++ javascript/potter/potter.js
@@ -0,0 +1,7 @@
+var Potter = function() {
+  this.price = function(books) {
+    return books.length * 8;
+  }
+}
+
+module.exports = Potter;
---
+++ javascript/potter/potter_test.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var Potter = require('./potter');
+
+describe('Potter', function() {
+  var potter = new Potter();
+
+  describe('#price()', function() {
+    it('returns correct price', function(){
+      assert.equal(0, potter.price([]));
+      assert.equal(8, potter.price([0]));
+      assert.equal(8, potter.price([1]));
+      assert.equal(8, potter.price([2]));
+      assert.equal(8, potter.price([3]));
+      assert.equal(8, potter.price([4]));
+      assert.equal(8 * 2, potter.price([0, 0]));
+      assert.equal(8 * 3, potter.price([1, 1, 1]));
+    })
+  })
+})
