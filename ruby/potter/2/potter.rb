896d8f0b19a556cac1971a5107e08bc321048b36
ErickFabian <fabianerickalfonso@gmail.com>
2015-07-08 11:26:13 -0500

    Refactor

--- ruby/potter/2/potter.rb
+++ ruby/potter/2/potter.rb
@@ -4,7 +4,6 @@ class Potter
 
   def initialize
     @book_price = 8
-    @total = 0
     @discount = {
       0 => 1,
       1 => 1,
@@ -16,32 +15,49 @@ class Potter
   end
 
   def price(books)
-    total = 0
-
     groups = sort_book_groups(books)
-    discount_groups(groups).each do |key, value|
-      total += book_price * value.count * discount[value.count]
-    end
-    total
+    discount_groups = build_discount_groups(groups)
+    calculate_total(discount_groups)
   end
 
-  def discount_groups(groups)
-    books_hash = Hash.new(0)
-    for i in 1..8 do
-      books_hash[group_number(i)] = groups.collect(&:shift).compact
-    end
-    books_hash
-  end
+  private
 
   def sort_book_groups(books)
-    groups = []
-    books.group_by{ |e| e }.each do |key, value|
-      groups << value
+    books.group_by{ |e| e }.map { |key, value| value }
+  end
+
+  def build_discount_groups(groups)
+    discount_groups = Hash.new(0)
+    for i in 1..highest_count_of_items(groups) do
+      discount_groups[group_number(i)] = groups.collect(&:shift).compact
     end
-    groups
+    discount_groups
+  end
+
+  def highest_count_of_items(groups)
+    max = 1
+    groups.each do |group|
+      if group.count > max
+        max = group.count
+      end
+    end
+    max
   end
 
   def group_number(key)
     "group_number_#{key}".to_sym
   end
+
+  def calculate_total(discount_groups)
+    total = 0
+    discount_groups.each do |key, books|
+      total += calculate_discount(books)
+    end
+    total
+  end
+
+  def calculate_discount(books)
+    book_price * books.count * discount[books.count]
+  end
+
 end
