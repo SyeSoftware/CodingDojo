ff5a830158f1371360e0ca08259e40a5afb63011
ErickFabian <fabianerickalfonso@gmail.com>
2015-07-08 10:41:02 -0500

    Resolve Tests

--- ruby/potter/2/potter.rb
+++ ruby/potter/2/potter.rb
@@ -1,5 +1,47 @@
 class Potter
+
+  attr_reader :book_price, :discount
+
+  def initialize
+    @book_price = 8
+    @total = 0
+    @discount = {
+      0 => 1,
+      1 => 1,
+      2 => 0.95,
+      3 => 0.9,
+      4 => 0.8,
+      5 => 0.75
+    }
+  end
+
   def price(books)
-    books.count * 8
+    total = 0
+
+    groups = sort_book_groups(books)
+    discount_groups(groups).each do |key, value|
+      total += book_price * value.count * discount[value.count]
+    end
+    total
+  end
+
+  def discount_groups(groups)
+    books_hash = Hash.new(0)
+    for i in 1..8 do
+      books_hash[group_number(i)] = groups.collect(&:shift).compact
+    end
+    books_hash
+  end
+
+  def sort_book_groups(books)
+    groups = []
+    books.group_by{ |e| e }.each do |key, value|
+      groups << value
+    end
+    groups
+  end
+
+  def group_number(key)
+    "group_number_#{key}".to_sym
   end
 end
--- ruby/potter/2/potter_test.rb
+++ ruby/potter/2/potter_test.rb
@@ -7,11 +7,26 @@ class PotterTest < Minitest::Test
     @potter = Potter.new
   end
 
-  def test_price
+  def test_basics
     assert_equal(0, @potter.price([]))
     assert_equal(8, @potter.price([0]))
     assert_equal(8, @potter.price([1]))
     assert_equal(8, @potter.price([2]))
     assert_equal(8 * 2, @potter.price([0, 0]))
+    assert_equal(8 * 3, @potter.price([1, 1, 1]))
+  end
+
+  def test_simple_discounts
+    assert_equal(8 * 2 * 0.95, @potter.price([0, 1]))
+    assert_equal(8 * 3 * 0.9, @potter.price([0, 2, 4]))
+    assert_equal(8 * 4 * 0.8, @potter.price([0, 1, 2, 4]))
+    assert_equal(8 * 5 * 0.75, @potter.price([0, 1, 2, 3, 4]))
+  end
+
+  def test_several_discounts
+    assert_equal(8 + (8 * 2 * 0.95), @potter.price([0, 0, 1]))
+    assert_equal(2 * (8 * 2 * 0.95), @potter.price([0, 0, 1, 1]))
+    assert_equal((8 * 4 * 0.8) + (8 * 2 * 0.95), @potter.price([0, 0, 1, 2, 2, 3]))
+    assert_equal(8 + (8 * 5 * 0.75), @potter.price([0, 1, 1, 2, 3, 4]))
   end
 end
