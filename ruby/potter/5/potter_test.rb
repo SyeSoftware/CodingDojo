ec9b90c5d4890f525ee6edb9d66c347c43194ff2
Rene Alfaro <rs.files.007@gmail.com>
2015-07-09 12:41:27 -0500

    I forgot to uncomment my latest tests

--- ruby/potter/5/potter_test.rb
+++ ruby/potter/5/potter_test.rb
@@ -22,16 +22,16 @@ class Test_Potter < Minitest::Test
   end
 
   def test_several_discounts
-    #assert_equal(8 + (8 * 2 * 0.95), Potter.new([0, 0, 1]).price)
-    #assert_equal(2 * (8 * 2 * 0.95), Potter.new([0, 0, 1, 1]).price)
-    #assert_equal((8 * 4 * 0.8) + (8 * 2 * 0.95), Potter.new([0, 0, 1, 2, 2, 3]).price)
-    #assert_equal(8 + (8 * 5 * 0.75), Potter.new([0, 1, 1, 2, 3, 4]).price)
+    assert_equal(8 + (8 * 2 * 0.95), Potter.new([0, 0, 1]).price)
+    assert_equal(2 * (8 * 2 * 0.95), Potter.new([0, 0, 1, 1]).price)
+    assert_equal((8 * 4 * 0.8) + (8 * 2 * 0.95), Potter.new([0, 0, 1, 2, 2, 3]).price)
+    assert_equal(8 + (8 * 5 * 0.75), Potter.new([0, 1, 1, 2, 3, 4]).price)
   end
 
   def test_edgeCases
-    #assert_equal(2 * (8 * 4 * 0.8), Potter.new([0, 0, 1, 1, 2, 2, 3, 4]).price)
-    #assert_equal(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8),
-    #Potter.new([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4]).price)
+    assert_equal(2 * (8 * 4 * 0.8), Potter.new([0, 0, 1, 1, 2, 2, 3, 4]).price)
+    assert_equal(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8),
+    Potter.new([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4]).price)
   end
 
 end
