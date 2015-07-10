c7d28cf40d9395af74735cb2c9f4af69074c3887
Rene Alfaro <rs.files.007@gmail.com>
2015-07-09 21:21:53 -0500

    des - plagiarism: basics and simple passed

--- ruby/potter/5/potter.rb
+++ ruby/potter/5/potter.rb
@@ -1,60 +1,33 @@
-class Potter
-  
-  attr_accessor :books
+class Potter  
+
   #Constants
-    BOOK_PRICE = 8
-    PERC_1 = 0.95
-    PERC_2 = 0.90
-    PERC_3 = 0.80
-    PERC_4 = 0.75
-  
-  def initialize(books)
-    @price = 0
-    @books = books || []    
+  BPRICE = 8
+  #Main invocation for returning the price
+  def price(books)
+    #Checks for an empty collection
+      return 0 if books.empty?
+      if books.uniq.size == 1
+        BPRICE * books.count
+      else
+        count_collection(books)
+      end
+
   end
 
-#Determines when the book collection should be submitted to a discount
-  def price
-    return 0 if books.empty?
-    if books.length == 1      
-      return books.size * BOOK_PRICE
-    else
-    #Dirty little trick that calculates when the edge cases occurs
-      begin
-        repeated_books = []
-        books.delete_if do |i|
-          unless repeated_books.include? i
-            repeated_books.push i
-          end
-        end
-        if books.length == 3 && repeated_books.length == 5
-          books.push(5)
-          repeated_books.pop
-        end
-        total_disscount(repeated_books.uniq.size)
-      end while books.any?
-    end
-    @price
+
+  def count_collection(books)
+    collection = books.clone.uniq.size
+    sum = 0    
+    case collection            
+         when 2
+           sum += BPRICE * collection * 0.95
+         when 3
+           sum += BPRICE * collection * 0.90
+         when 4
+           sum += BPRICE * collection * 0.80
+         when 5
+           sum += BPRICE * collection * 0.75
+         end     
   end
 
-  #Main disscount of calculations for the givven cart.
-  def total_disscount(books)
-    @price += BOOK_PRICE * books * available_disscounts(books)
-  end
-
-  # Return 1 if the size of the array
-  def available_disscounts(books)
-    case books
-      when 1
-        return 1
-      when 2
-        PERC_1
-      when 3
-        PERC_2
-      when 4
-        PERC_3
-      when 5
-        PERC_4
-      end         
-  end
 end
--- ruby/potter/5/potter_test.rb
+++ ruby/potter/5/potter_test.rb
@@ -2,36 +2,41 @@ require "minitest/autorun"
 require './potter'
 
 class Test_Potter < Minitest::Test
+  def setup
+    @potter = Potter.new
+  end
 
-  def test_price_basic
-    assert_equal(0, Potter.new([]).price)
-    assert_equal(8, Potter.new([0]).price)
-    assert_equal(8, Potter.new([1]).price)
-    assert_equal(8, Potter.new([2]).price)
-    assert_equal(8, Potter.new([3]).price)
-    assert_equal(8, Potter.new([4]).price)
-    assert_equal(8 * 2 , Potter.new([0, 0]).price)
-    assert_equal(8 * 3, Potter.new([1, 1, 1]).price)
+  def test_basics
+    assert_equal(0, @potter.price([]))
+    assert_equal(8, @potter.price([0]))
+    assert_equal(8, @potter.price([0]))
+    assert_equal(8, @potter.price([0]))
+    assert_equal(8, @potter.price([0]))
+    assert_equal(8, @potter.price([0]))
+
+    assert_equal(8 * 2, @potter.price([0, 0]))
+    assert_equal(8 * 3, @potter.price([0, 0, 0]))
   end
 
   def test_simple_discounts
-    assert_equal(8 * 2 * 0.95, Potter.new([0, 1]).price)
-    assert_equal(8 * 3 * 0.9, Potter.new([0, 2, 4]).price)
-    assert_equal(8 * 4 * 0.8, Potter.new([0, 1, 2, 4]).price)
-    assert_equal(8 * 5 * 0.75, Potter.new([0, 1, 2, 3, 4]).price)
+    assert_equal(8 * 2 * 0.95, @potter.price([0, 1])) #15.2
+    assert_equal(8 * 3 * 0.90, @potter.price([0, 1, 2])) #21.6
+    assert_equal(8 * 4 * 0.80, @potter.price([0, 1, 2, 3])) #25.6
+    assert_equal(8 * 5 * 0.75, @potter.price([0, 1, 2, 3, 4])) #30.0
+
   end
 
   def test_several_discounts
-    assert_equal(8 + (8 * 2 * 0.95), Potter.new([0, 0, 1]).price)
-    assert_equal(2 * (8 * 2 * 0.95), Potter.new([0, 0, 1, 1]).price)
-    assert_equal((8 * 4 * 0.8) + (8 * 2 * 0.95), Potter.new([0, 0, 1, 2, 2, 3]).price)
-    assert_equal(8 + (8 * 5 * 0.75), Potter.new([0, 1, 1, 2, 3, 4]).price)
+    assert_equal(8 + (8 * 2 * 0.95), @potter.price([0, 0, 1])) #23.2
+    #assert_equal(2 * (8 * 2 * 0.95), @potter.price({b1: 2, b2: 2, b3: 0, b4: 0, b5: 0})) #30.4
+    #assert_equal((8 * 4 * 0.8) + (8 * 2 * 0.95), @potter.price({b1: 2, b2: 1, b3: 2, b4: 1, b5: 0})) #40.8
+    #assert_equal(8 + (8 * 5 * 0.75), @potter.price({b1: 1, b2: 2, b3: 1, b4: 1, b5: 1})) #38
   end
 
-  def test_edgeCases
-    assert_equal(2 * (8 * 4 * 0.8), Potter.new([0, 0, 1, 1, 2, 2, 3, 4]).price)
+  def edge_cases
+    assert_equal(2 * (8 * 4 * 0.80), @potter.price({b1: 2, b2: 2, b3: 2, b4: 1, b5: 1}))
     assert_equal(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8),
-    Potter.new([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4]).price)
+    @potter.price({b1: 5, b2: 5, b3: 4, b4: 5, b5: 4}))
   end
 
 end
